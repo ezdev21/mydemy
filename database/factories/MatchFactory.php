cd845d09b28040759b454fcef8c0b68183f93470
gech11 <getachewfikadu77@gmail.com>
2021-05-16 15:54:07 +0300

    initial commit

---
+++ database/factories/MatchFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Match;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MatchFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Match::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
