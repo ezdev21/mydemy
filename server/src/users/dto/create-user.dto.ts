d9bc57bb9919031dcbbf746c1efce812b811772a
ezra <ezrafikadu02@gmail.com>
2023-05-02 16:20:26 +0300

    data validation and transformation

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsNotEmpty } from "class-validator"
+
 export class CreateUserDto {
+    @IsNotEmpty()
     name: string
+
+    @IsNotEmpty()
+    @IsEmail()
     email: string
 }
