da32e089cc4a672f8a69311582beff8aa9f86d08
ezra <ezrafikadu02@gmail.com>
2023-10-29 16:29:15 +0300

    prisma integrated

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mongodb',
-    database: 'mydemy',
-    entities: ['dist/**/*/entity.ts'],
-    // synchronize: true
-  }), UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,PrismaService],
 })
 export class AppModule {}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,36 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User) private usersRepository: Repository<User>
-  ){}
+  constructor(private prisma: PrismaService ){}
 
   create(createUserDto: CreateUserDto) {
-    const newUser = this.usersRepository.create(createUserDto)
-    return this.usersRepository.save(newUser);
+    
   }
 
   findAll() {
-    return this.usersRepository.find();
+    return this.prisma.user.findMany()
   }
 
   findOne(id: number) {
-    return this.usersRepository.findOneBy({id});
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
     const user = await this.findOne(id)
-    return this.usersRepository.save({...user,...updateUserDto});
   }
 
   async remove(id: number) {
     const user = await this.findOne(id)
-    return this.usersRepository.remove(user);
   }
 }
