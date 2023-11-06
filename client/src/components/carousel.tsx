675e4a0069a8e6b5d9cf98af496d26292e69526a
ezra <ezrafikadu02@gmail.com>
2023-11-07 02:29:13 +0300

    homepage carousel

---
+++ client/src/components/carousel.tsx
@@ -0,0 +1,63 @@
+import { useEffect } from "react"
+
+ const CarouselDefault = ()=>{
+    if(typeof window !== 'undefined') {
+        const buttons = document.querySelectorAll("[data-carousel-button]")
+        buttons.forEach(button => {
+        button.addEventListener("click", () => {
+          const offset = button.dataset.carouselButton === "next" ? 1 : -1
+          const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+          const activeSlide = slides.querySelector("[data-active]")
+          let newIndex = [...slides.children].indexOf(activeSlide) + offset
+          if (newIndex < 0) newIndex = slides.children.length - 1
+          if (newIndex >= slides.children.length) newIndex = 0
+          slides.children[newIndex].dataset.active = true
+          delete activeSlide.dataset.active
+        })
+      })  
+      useEffect(()=>{
+        setInterval(()=>{
+            const slides = document.querySelector("[data-slides]")
+            const activeSlide = slides?.querySelector("[data-active]")
+            let newIndex = [...slides?.children].indexOf(activeSlide) + 1
+            if (newIndex < 0) newIndex = slides.children.length - 1
+            if (newIndex >= slides?.children.length) newIndex = 0
+            slides.children[newIndex].dataset.active = true
+            delete activeSlide?.dataset.active
+        },4000)
+    })
+    }
+  return (
+    <div className="carousel  text-4xl text-white" data-carousel>
+      <button className="carousel-button prev outline-none" data-carousel-button="prev">&lsaquo;</button>
+      <button className="carousel-button next" data-carousel-button="next">&rsaquo;</button>
+      <ul data-slides>
+        <li className="slide relative" data-active>
+          <div className="absolute inset-0 flex justify-center items-center">
+            <h1 className="w-1/2">Learn creative skills to achieve your personal and professional goals.</h1>
+          </div>  
+          <img 
+            src="https://media.istockphoto.com/id/1320160546/nl/foto/jongen-die-thuis-e-learning-bestudeert.jpg?s=612x612&w=0&k=20&c=aEPDGyEb1Q3pfYlWXUvkOQPSNuHuInHiM1cDHjmpQIs=" 
+            alt="Nature Image #1"/>
+        </li>
+        <li className="slide relative">
+          <div className="absolute inset-0 flex justify-center items-center">
+            <h1 className="w-1/2">Tune in and level up at your own pace</h1>
+          </div> 
+          <img 
+            src="https://cdn.pixabay.com/photo/2019/05/20/13/15/webinar-4216601_640.jpg"
+            alt="Nature Image #2"/>
+        </li>
+        <li className="slide relative">
+          <div className="absolute inset-0 flex justify-center items-center">
+            <h1 className="w-1/2">Connect with a global community of curious creatives</h1>
+          </div> 
+          <img 
+            src="https://cdn.pixabay.com/photo/2020/05/02/12/38/homeschooling-5121262_1280.jpg"
+            alt="Nature Image #3"/>
+        </li>
+      </ul>
+    </div>
+  );
+}
+export default CarouselDefault
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { FormEvent, useState, useEffect } from "react";
 import {useRouter} from 'next/router'
 import axios from '../libs/axios'
-
+import CarouselDefault from "../components/carousel";
 const Home = () => {
   const router = useRouter()
   const [type,setType]=useState<string>('');
@@ -35,18 +35,18 @@ const Home = () => {
         <a href="/" className='text-xl'>Mydemy</a>
         <form onSubmit={search}>
           <div className="mx-3 p-1 rounded-3xl bg-gray-200 px-5 border-2 border-primary flex items-stretch">
-            <select onChange={(e)=>setType(e.target.value)} v-model="categoryId" required className="bg-gray-200 px-5">
-              <option value="" disabled>Any</option>
+            <select onChange={(e)=>setType(e.target.value)} v-model="categoryId" className="bg-gray-200 px-5">
+              <option value="">Any</option>
               <option value="course">Course </option>
               <option value="instructor">Instructor</option>
             </select>
-            <select onChange={(e)=>setCategory(e.target.value)} v-model="categoryId" required className="bg-gray-200 px-5">
-              <option value="all" disabled>Category</option>
+            <select onChange={(e)=>setCategory(e.target.value)} v-model="categoryId" className="bg-gray-200 px-5">
+              <option value="all">Category</option>
               {categories?.map((category:any)=>(
                 <option key={category.id} value={category.name}>{category.name}</option>
               ))}
             </select>
-            <input type="text" onChange={(e)=>setQuery(e.target.value)} required placeholder="search anything" className="px-2 bg-gray-200 w-60 rounded outline-none placeholder-gray-700"/>
+            <input type="search" onChange={(e)=>setQuery(e.target.value)} required placeholder="search anything" className="px-2 bg-gray-200 w-60 rounded outline-none placeholder-gray-700 focus:border-0"/>
             <button type="submit" className="text-primary text-xl bg-gray-200 lg:mx-1 xl:mx-1 2xl:mx-1">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -62,87 +62,8 @@ const Home = () => {
           <a href="/signup" className='text-white border-2 border-primary bg-primary px-5 py-1 font-bold rounded-md'>Sign up</a>
         </div>
       </div>
-      <div className="">
-        <div
-        id="carouselExampleControls"
-        className="relative"
-        data-te-carousel-init
-        data-te-carousel-slide>
-        <div
-          className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-        <div
-          className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item
-          data-te-carousel-active>
-          <img
-            src="https://media.istockphoto.com/id/1320160546/nl/foto/jongen-die-thuis-e-learning-bestudeert.jpg?s=612x612&w=0&k=20&c=aEPDGyEb1Q3pfYlWXUvkOQPSNuHuInHiM1cDHjmpQIs="
-            className="block w-full h-72 object-cover"
-            alt="Image 1" />
-        </div>
-        <div
-          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item>
-          <img
-            src="https://cdn.pixabay.com/photo/2019/05/20/13/15/webinar-4216601_640.jpg"
-            className="block h-64"
-            alt="Image 2" />
-        </div>
-        <div
-          className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-          data-te-carousel-item>
-          <img
-            src="https://cdn.pixabay.com/photo/2020/05/02/12/38/homeschooling-5121262_1280.jpg"
-            className="block h-64"
-            alt="Image 3" />
-        </div>
-      </div>
-      <button
-        className="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-te-target="#carouselExampleControls"
-        data-te-slide="prev">
-        <span className="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-        </span>
-        <span
-          className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Previous</span>
-      </button>
-      <button
-        className="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-        type="button"
-        data-te-target="#carouselExampleControls"
-        data-te-slide="next">
-        <span className="inline-block h-8 w-8">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-6 w-6">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </span>
-        <span
-          className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-          >Next
-        </span>
-        </button>
-        </div>
+      <div className="h-1/2 overflow-clip">
+      <CarouselDefault/>
       </div>
       <div className='text-center bg-white p-3 my-3 mx-5'>
         <h1 className='text-2xl font-bold'>A broad selection of courses</h1>
@@ -168,7 +89,7 @@ const Home = () => {
            <p className='text-xl'>Instructors from around the world teach millions of students on Our. We provide the tools and skills to teach what you love.</p>
         </div>
       </div>
-      <footer className='bg-gray-800 p-5'>
+      <footer className='bg-primary p-5'>
              
       </footer>
   </div>
