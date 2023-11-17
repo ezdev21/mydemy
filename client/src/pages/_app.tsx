a64b796af0daf13fb2bbcaf0522337394eee1b01
ezra <ezrafikadu02@gmail.com>
2023-11-17 07:10:16 -0800

    redux integrated

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,8 +2,11 @@ import '@/styles/globals.css'
 import '../styles/sidebar.css'
 import '../styles/carousel.css'
 
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { wrapper } from "../store/store";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+export default wrapper.withRedux(App);
